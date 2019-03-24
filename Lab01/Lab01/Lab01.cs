1fad7f4f1b2ac7cfcb9f60b0579e782ba85ef47c
Kevin Mizway <S00086606@acad.tri-c.edu>
2019-03-24 16:38:15 -0400

    Completed Lab 1

---
+++ Lab01/Lab01/Lab01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Lab01
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World from Kevin Mizway");
+        }
+    }
+
